cc0a542c4a634950e3effc39cc03e116f4312ce2
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-05-24 01:25:32 +0700

    Bug Fix

--- index.php
+++ index.php
@@ -26,4 +26,6 @@ class InstallerPlugin implements PluginInterface {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+    public function deactivate(Composer $composer, IOInterface $io) {}
+    public function uninstall(Composer $composer, IOInterface $io) {}
 }
