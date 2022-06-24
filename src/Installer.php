cbccf8cfc108d340990055556af83847de1f23f4
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-06-24 22:10:39 +0700

    Update

--- src/Installer.php
+++ src/Installer.php
@@ -2,6 +2,11 @@
 
 namespace Mecha\Composer;
 
+use const DIRECTORY_SEPARATOR;
+
+use function basename;
+use function substr;
+
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 
@@ -9,10 +14,10 @@ class Installer extends LibraryInstaller {
     public function getInstallPath(PackageInterface $package) {
         $name = basename($package->getPrettyName());
         if ('x.' === substr($name, 0, 2)) {
-            return 'lot/x/' . substr($name, 2);
+            return 'lot' . DIRECTORY_SEPARATOR . 'x' . DIRECTORY_SEPARATOR . substr($name, 2);
         }
         if ('y.' === substr($name, 0, 2)) {
-            return 'lot/y/' . substr($name, 2);
+            return 'lot' . DIRECTORY_SEPARATOR . 'y' . DIRECTORY_SEPARATOR . substr($name, 2);
         }
         return parent::getInstallPath($package);
     }
--- src/Plugin.php
+++ src/Plugin.php
@@ -2,27 +2,44 @@
 
 namespace Mecha\Composer;
 
+use const DIRECTORY_SEPARATOR;
+use const JSON_UNESCAPED_SLASHES;
+use const JSON_UNESCAPED_UNICODE;
+
+use function basename;
+use function dirname;
+use function json_decode;
+use function json_encode;
+use function rmdir;
+use function strpos;
+use function unlink;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
-
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
+use Composer\Script\ScriptEvents;
 
 class Plugin implements PluginInterface, EventSubscriberInterface {
     public function activate(Composer $composer, IOInterface $io) {
-        $installer = new Installer($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $composer->getInstallationManager()->addInstaller($this->installer = new Installer($io, $composer));
     }
     public function deactivate(Composer $composer, IOInterface $io) {}
-    public function onPostCreateProjectCommand(Event $event) {
-        return $this->onPostUpdateCommand($event);
-    }
-    public function onPostUpdateCommand(Event $event) {
-        $dir = new RecursiveDirectoryIterator($r = getcwd(), RecursiveDirectoryIterator::SKIP_DOTS);
+    public function onPostCreateProject(Event $event) {
+        $r = dirname($event->getComposer()->getConfig()->get('vendor-dir'), 2);
+        $dir = new RecursiveDirectoryIterator($r, RecursiveDirectoryIterator::SKIP_DOTS);
         $files = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::CHILD_FIRST);
+        $files_to_delete = [
+            '.editorconfig' => 1,
+            '.gitattributes' => 1,
+            '.gitignore' => 1,
+            '.gitmodules' => 1,
+            'README.md' => 1
+        ];
         foreach ($files as $v) {
             $path = $v->getRealPath();
             if (
@@ -33,23 +50,25 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
             ) {
                 $v->isDir() ? rmdir($path) : unlink($path);
             }
+            if ($v->isFile()) {
+                if (isset($files_to_delete[$n = basename($path)])) {
+                    unlink($path);
+                }
+                // Minify `composer.json` and `composer.lock`
+                if ('composer.json' === $n || 'composer.lock' === $n) {
+                    file_put_contents($path, json_encode(json_decode(file_get_contents($path)), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+                }
+            }
         }
-        is_file($path = $r . DIRECTORY_SEPARATOR . '.gitignore') && unlink($path);
-        is_file($path = $r . DIRECTORY_SEPARATOR . '.gitmodules') && unlink($path);
-        is_file($path = $r . DIRECTORY_SEPARATOR . 'README.md') && unlink($path);
-        // Minify `composer.json` and `composer.lock`
-        if (is_file($path = $r . DIRECTORY_SEPARATOR . 'composer.json')) {
-            file_put_contents($path, json_encode(json_decode(file_get_contents($path)), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
-        }
-        if (is_file($path = $r . DIRECTORY_SEPARATOR . 'composer.lock')) {
-            file_put_contents($path, json_encode(json_decode(file_get_contents($path)), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
-        }
+    }
+    public function onPostUpdate(Event $event) {
+        return $this->onPostCreateProject($event);
     }
     public function uninstall(Composer $composer, IOInterface $io) {}
     public static function getSubscribedEvents() {
         return [
-            'post-create-project-cmd' => 'onPostCreateProjectCommand',
-            'post-update-cmd' => 'onPostUpdateCommand'
+            ScriptEvents::POST_CREATE_PROJECT_CMD => 'onPostCreateProject',
+            ScriptEvents::POST_UPDATE_CMD => 'onPostUpdate'
         ];
     }
 }
