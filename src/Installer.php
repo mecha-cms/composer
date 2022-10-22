99d83fd23ab44b2d18522dad902086beb385bf2b
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-22 19:28:16 +0700

    Dirty Fix

--- src/Installer.php
+++ src/Installer.php
@@ -5,15 +5,17 @@ namespace Mecha\Composer;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 
-class Installer extends LibraryInstaller {
-    public function getInstallPath(PackageInterface $package) {
-        $name = \basename($package->getPrettyName());
-        if ('x.' === \substr($name, 0, 2)) {
-            return 'lot' . \DIRECTORY_SEPARATOR . 'x' . \DIRECTORY_SEPARATOR . \substr($name, 2);
+if (!\class_exists(__NAMESPACE__ . "\\Installer")) {
+    class Installer extends LibraryInstaller {
+        public function getInstallPath(PackageInterface $package) {
+            $name = \basename($package->getPrettyName());
+            if ('x.' === \substr($name, 0, 2)) {
+                return 'lot' . \DIRECTORY_SEPARATOR . 'x' . \DIRECTORY_SEPARATOR . \substr($name, 2);
+            }
+            if ('y.' === \substr($name, 0, 2)) {
+                return 'lot' . \DIRECTORY_SEPARATOR . 'y' . \DIRECTORY_SEPARATOR . \substr($name, 2);
+            }
+            return parent::getInstallPath($package);
         }
-        if ('y.' === \substr($name, 0, 2)) {
-            return 'lot' . \DIRECTORY_SEPARATOR . 'y' . \DIRECTORY_SEPARATOR . \substr($name, 2);
-        }
-        return parent::getInstallPath($package);
     }
 }
